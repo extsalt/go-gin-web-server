30dd759c4ce30214db89fc05509562c709cb9086
Ganesh Gupta <45989209+extsalt@users.noreply.github.com>
2023-08-13 13:36:59 +0000

    Clean up

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 
 func main() {
 	ConfigRuntime()
-	StartWorkers()
 	StartGin()
 }
 
@@ -22,24 +21,12 @@ func ConfigRuntime() {
 	fmt.Printf("Running with %d CPUs\n", nuCPU)
 }
 
-// StartWorkers start starsWorker by goroutine.
-func StartWorkers() {
-	go statsWorker()
-}
 
 // StartGin starts gin web server with setting router.
 func StartGin() {
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.New()
-	router.Use(rateLimit, gin.Recovery())
-	router.LoadHTMLGlob("resources/*.templ.html")
-	router.Static("/static", "resources/static")
-	router.GET("/", index)
-	router.GET("/room/:roomid", roomGET)
-	router.POST("/room-post/:roomid", roomPOST)
-	router.GET("/stream/:roomid", streamRoom)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
